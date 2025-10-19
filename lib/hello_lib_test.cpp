b6cc54ec7a33d74b6d14a69999eb7a29701ff78a
Chikee <royallor@163.com>
2025-10-19 19:07:38 +0800

    integrate googletest

---
+++ lib/hello_lib_test.cpp
@@ -0,0 +1,7 @@
+#include "lib/hello_lib.hpp"
+#include <gtest/gtest.h>
+
+TEST(HelloLibTest, BasicGreeting) {
+  std::string s = GetGreeting("Bazel");
+  EXPECT_EQ(s, "Hello, Bazel power by format!");
+}
